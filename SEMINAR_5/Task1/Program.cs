17c932075997fd2ed1373ed7905a92b9359c4531
Tatyana <tatyana.kurii@mail.ru>
2024-04-29 14:33:08 +0600

    add Seminsr_5:task_1

---
+++ SEMINAR_5/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Основной скелет для написания программы (создание двумерного массива)
+
+void Main()
+{
+  int row = ReadInt("Введите количество строк: "); //1. сколько будет строк
+  int column = ReadInt("Введите количество столбцов: ");  //1. сколько будет столбцов
+  int[,] myMatrix = GenerateMatrix(row, column, 0, 9);
+  PrintMatrix(myMatrix);
+}
+
+void PrintMatrix(int[,] matrix) //5. выводит матрицу
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write(matrix[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+int[,] GenerateMatrix(int row, int col, int minRange, int maxRange) //2. создаем метод, который сгенерирует массив
+{
+  int[,] tempMatrix = new int[row, col]; //3. создаем временную матрицу
+  Random rand = new Random(); //4. создаем одну единственную ячейку, в которой будет проходить генерация чмсла
+
+  for (int i = 0; i < tempMatrix.GetLength(0); i++) //
+    for (int j = 0; j < tempMatrix.GetLength(1); j++)
+      tempMatrix[i, j] = rand.Next(minRange, maxRange + 1);
+
+  return tempMatrix;
+}
+
+int ReadInt(string msg) //1. принимаем размер нашего массива
+{
+  System.Console.Write(msg);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
+
---
+++ SEMINAR_5/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задание 1. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти 
+// элементы на их квадраты.
+// Пример
+// 2 3 4 3       4 3 16 3
+//  4 3 4 1 =>   4 3 4 1
+//  2 9 5 4      4 9 25 4
+
+void Main()
+{
+  int row = ReadInt("Введите количество строк: ");
+  int column = ReadInt("Введите количество столбцов: ");
+  int[,] myMatrix = GenerateMatrix(row, column, 0, 9);
+  PrintMatrix(myMatrix);
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write(matrix[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+int[,] GenerateMatrix(int row, int col, int minRange, int maxRange)
+{
+  int[,] tempMatrix = new int[row, col];
+  Random rand = new Random();
+
+  for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    for (int j = 0; j < tempMatrix.GetLength(1); j++)
+      tempMatrix[i, j] = rand.Next(minRange, maxRange + 1);
+
+  return tempMatrix;
+}
+
+int ReadInt(string msg)
+{
+  System.Console.Write(msg);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
