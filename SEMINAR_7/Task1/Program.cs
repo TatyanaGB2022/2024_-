d595c67ececb8675b36d8c409fbdd9d980de4a6e
Tatyana <tatyana.kurii@mail.ru>
2024-05-01 20:40:41 +0600

    add Seminar_7:task1

--- SEMINAR_7/Task1/Program.cs
+++ SEMINAR_7/Task1/Program.cs
@@ -6,3 +6,22 @@
 // 123 => 6
 // 63 => 9
 
+void Main()
+{
+  int number = ReadInt("Введите число: ");
+ System.Console.WriteLine(SumOfDigits(number));
+}
+
+int SumOfDigits(int N)
+{
+  if(N < 1) return 0;
+  return N % 10 + SumOfDigits(N / 10);
+}
+
+int ReadInt(string msg)
+{
+  System.Console.Write(msg);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
